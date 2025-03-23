4bd1b5d36d5ffecb1f91b252a0fc8891b1678db4
abhishek-Roy22 <100830257+abhishek-Roy22@users.noreply.github.com>
2025-03-23 13:47:39 +0530

    created everything

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -30,7 +31,29 @@ class AuthController extends Controller
         return redirect()->route('ninjas.index');
     }
 
-    public function login() {
-        
+    public function login(Request $request) {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if (Auth::attempt($validated)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('ninjas.index');
+        }
+
+        throw ValidationException::withMessages([
+            'credentials' => 'Sorry, incorrect credentials'
+        ]);
+    }
+
+    public function logout (Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('show.login');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,15 +17,27 @@
   <header>
     <nav>
       <h1>Ninja Network</h1>
+      
+      @guest
+        <a href="{{ route('show.login') }}" class="btn">Login</a>
+        <a href="{{ route('show.register') }}" class="btn">Register</a>
+      @endguest
+
+      @auth
       <a href="{{ route('ninjas.index') }}"> 
         All Ninjas
       </a>
-      <a href="{{ route('ninjas.create') }}">
-        Create New Ninja
-      </a>
-
-      <a href="{{ route('show.login') }}" class="btn">Login</a>
-      <a href="{{ route('show.register') }}" class="btn">Register</a>
+        <span class="border-r-2 pr-2">
+          Hi there, {{ Auth::user()->name }}
+        </span>
+        <a href="{{ route('ninjas.create') }}">
+          Create New Ninja
+        </a>
+        <form action="{{route('logout')}}" method="POST" class="m-0">
+          @csrf
+          <button class="btn">Logout</button>
+        </form>
+      @endauth
     </nav>
   </header>
 
